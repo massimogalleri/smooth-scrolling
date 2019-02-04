a64d76d8414e38639b473693f3f828575f51f7a5
Massimo Galleri <massimo@galleri.it>
2019-02-04 13:27:43 +0100

    Update smooth-scrolling.js

--- smooth-scrolling.js
+++ smooth-scrolling.js
@@ -1,22 +1,24 @@
-var scrollTime = 0.2,
-  scrollDistance = 50;
+{
+    const scrollTime = 0.2;
+    const scrollDistance = 50;
 
-window.addEventListener("wheel", event => {
+    window.addEventListener("wheel", event => {
 
-  event.preventDefault();
+      event.preventDefault();
 
-  var delta = event.deltaY / 100 || -event.deltaFactor / 3,
-    scrollTop = window.pageYOffset,
-    finalScroll = scrollTop - parseInt(delta * scrollDistance);
+      let delta = event.deltaY / 100 || -event.deltaFactor / 3;
+      let scrollTop = window.pageYOffset;
+      let finalScroll = scrollTop - parseInt(delta * scrollDistance);
 
-  TweenMax.to(window, scrollTime, {
-    scrollTo: {
-      y: finalScroll,
-      autoKill: true
-    },
-    ease: Power2.easeOut,
-    autoKill: true,
-    overwrite: 5
-  });
+      TweenMax.to(window, scrollTime, {
+        scrollTo: {
+          y: finalScroll,
+          autoKill: true
+        },
+        ease: Power2.easeOut,
+        autoKill: true,
+        overwrite: 5
+      });
 
-});
+    });
+}
